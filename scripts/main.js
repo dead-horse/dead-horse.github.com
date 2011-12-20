34016538f3c3a8c5ad82c0f88d386c9d39e31e40
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-20 10:45:59 +0800

    add cloud

--- scripts/main.js
+++ scripts/main.js
@@ -38,3 +38,40 @@ function bindClick(){
     }
   })
 }
+/***
+* 生成标签云的tags
+*/
+function getCloud(){
+  var words = [];
+  var arrKeyWords = $('.post-keywords');
+  //get all the key words
+  for(var i=0, len=arrKeyWords.length; i!=len; ++i){
+    var keyWords = $(arrKeyWords[i]);
+    var words.concat(keyWords.html().split(','));
+  }
+  //get the tags {keywords:num}
+  var tags= {};
+  for(var i=0, len=words.length; i!=length; ++i){
+    var word = words[i];
+    tags[word] = tags[word] ? tags[word]+1 : 1;
+  }
+  //genarate the cloud element
+  var minSize = 10, maxSize = 15, maxNum=-1, minNum=10000;
+  words = [];
+  for(var key in tags){
+    var num = tags[key];
+    maxNum = num>maxNum ? num : maxNum;
+    minNum = num<minNum ? num : minNum;
+  }
+  var times = (maxSize-minSize)/(maxNum-minNum);
+  var htmls = [];
+  for(var key in tags){
+    var size = math.ceil((tags[key]-1)*times);
+    htmls.push('<a href="javascript:void(0) class="key-word" style="font-size:');
+    htmls.push(size);
+    htmls.push('px">');
+    htmls.push(key);
+    htmls.push('</a>');
+  }
+  $('#cloud-tags').html(htmls.join(''));
+}
